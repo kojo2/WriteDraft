b7fd5fb4d0e153f435c8db64cb99674e27ca93a8
Matt <m.hutfield@hotmail.com>
2022-10-23 11:14:00 +0100

    add autosave and autoload

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,8 +1,31 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
+import useInterval from 'use-interval'
 import Deck from './Components/Deck'
 import TextEditor from './Components/TextEditor'
+import { updateCards, updateDrafts } from './redux/mainActions'
+import useRedux from './redux/useRedux'
 
 const AppWrapper = () => {
+  const { cards, drafts } = useRedux()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    try {
+      let s = localStorage.getItem('stuff')
+      let o = JSON.parse(s)
+      dispatch(updateCards(o.cards))
+      dispatch(updateDrafts(o.drafts))
+    } catch (err) {
+      console.log("couldn't load the stuff")
+    }
+  }, [])
+
+  useInterval(() => {
+    localStorage.setItem('stuff', JSON.stringify({ cards, drafts }))
+  }, [120000])
+
   return (
     <div className="App-container">
       <Routes>
